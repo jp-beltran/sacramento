d3a883dcc939692884d09aca8efc0bb1b5a7a38d
joao <jpbcs2005@gmail.com>
2025-05-09 16:20:48 -0300

    gif update

--- src/pages/OndeAHistoria.tsx
+++ src/pages/OndeAHistoria.tsx
@@ -6,7 +6,7 @@ import { useLanguage } from "../context/LanguageContext";
 import topheader from "../assets/backgroundHeaderFooter.svg";
 import logo from "../assets/LogoBranca.svg";
 import menuIcon from "../assets/MenuIcoWhite.svg";
-import gifHeader from "../assets/images/pg1/gif.mp4";
+import gifHeader from "../assets/images/pg1/gifHeader.webm";
 import img1 from "../assets/images/pg1/img1.webp";
 
 // Lazy load dos componentes
